b3ca869559505855ee317105756578afa11aacd1
tomekg <tomek.groos@gmail.com>
2019-12-21 10:52:49 +0100

    testowy commit na branchu JS21grudzien

--- src/cw23/cw23.js
+++ src/cw23/cw23.js
@@ -1,25 +1,30 @@
-function testJs() {
+class car {
+    constructor(brand, model, productionYear, color) {
+        this.brand = brand;
+        this.model = model;
+        this.productionYear = productionYear;
+        this.color = color;
 
-    class car {
-        constructor(brand, model, productionYear, color) {
-            this.brand = brand;
-            this.model = model;
-            this.productionYear = 0;
-            this.color = color;
-
-        }
-
-        displayBrandAndModel(){
-            console.log("Marka: " + this.brand + "Model: " + this.model);
-        }
-
-        drive() {
-            console.log("Teraz prowadzę " + this.brand);
-        }
     }
 
-    let firstCar = new car("Volvo", "S60", 2010, "Red");
+    displayBrandAndModel() {
+        console.log("Marka: " + this.brand + "Model: " + this.model);
+    }
 
-        console.log(firstCar);
+    drive() {
+        console.log("Driving");
+    }
 }
+
+function createCar() {
+    let fiat500 = new car("Fiat", "500", 2010, "White");
+    fiat500.displayBrandAndModel();
+    console.log("Rok produkcji: " + fiat500.productionYear);
+    console.log("Kolor: " + fiat500.color);
+}
+
+
+
+
+
 
