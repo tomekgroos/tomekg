e999b7106b0c68fee853a609335b04b16cffbf54
tomekg <tomek.groos@gmail.com>
2019-12-21 11:38:47 +0100

    testowy commit na branchu JS21grudzien

---
+++ src/Ex26/ex26.js
@@ -0,0 +1,7 @@
+function testScreenObject() {
+
+    document.getElementById("demo").innerHTML =
+        "Screen height is: " + screen.availHeight + " Screen width is: " + screen.availWidth;
+
+
+}
---
+++ src/cw24/cw24.js
@@ -0,0 +1,14 @@
+function testScreenObject() {
+
+    document.getElementById("avail").innerHTML =
+        "Screen height is: " + screen.availHeight + " Screen width is: " + screen.availWidth;
+
+       document.getElementById("height").innerHTML = "Wysokosc: " + screen.height;
+
+       document.getElementById("width").innerHTML =  "Szerokosc: " + screen.width;
+
+       document.getElementById("pxdepth").innerHTML =  "Glebia koloru: " + screen.colorDepth;
+
+
+
+}
