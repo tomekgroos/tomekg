142189f4fb34dc0ef6d297bbadee06e3cd0ed844
tomekg <tomek.groos@gmail.com>
2019-12-21 13:02:21 +0100

    po przykladzie 29

--- src/Ex29/ex29.js
+++ src/Ex29/ex29.js
@@ -1,7 +1,7 @@
 function testPrompt() {
 
    var txt;
-   var person = prompt("Wprowadz imie: ", "Jan Kowalski");
+   var person = prompt("Wprowadz swoje imie: ", "Tomek Groos");
 
    if (person == null || person == ""){
       txt = "Anulowano przez uzytkownika";
