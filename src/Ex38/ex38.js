50f2be5891f64f2dd7876de25e15d9207944b4ed
tomekg <tomek.groos@gmail.com>
2019-12-22 10:54:19 +0100

    rozwiazane cwiczenie 27

--- src/Ex38/ex38.js
+++ src/Ex38/ex38.js
@@ -1,28 +1,5 @@
 
-var x = document.getElementById("myBtn");
-
-x.addEventListener("mouseover", myFunction);
-x.addEventListener("mouseout", myThirdFunction);
-x.addEventListener("click", mySecondFunction);
-
-x.removeEventListener()
-
-
-     function myFunction() {
-         document.getElementById("demo").innerHTML += "Mouse Over!<br>";
-
-     }
-
-     function mySecondFunction() {
-         document.getElementById("demo").innerHTML += "Mouse Clicked!<br>";
-         x.removeEventListener("click", mySecondFunction);
-     }
-
-     function myThirdFunction() {
-         document.getElementById("demo").innerHTML += "Mouse Out!<br>";
-
-     }
-
+$('body').append("Witaj");
 
 
 
