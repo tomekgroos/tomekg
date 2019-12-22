a304dd084e1e413b1e8377b5837a68cfd4e7d9cf
tomekg <tomek.groos@gmail.com>
2019-12-22 12:58:32 +0100

    po przykladzie 44

--- src/Ex44/app.js
+++ src/Ex44/app.js
@@ -1,10 +1,18 @@
 
+let app = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/xml", "Access-Control-Allow-Origin": "*"});
+
 function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
 
-    if(this.readyState == 4 && this.status == 200){
-        myFunction(this);
-    }
+            myFunction(this);
+        }
+    };
+    xmlhttp.open("GET", "http://localhost:3000/");
+    xmlhttp.send();
+
 };
 
 function myFunction(xml) {
@@ -13,12 +21,17 @@ function myFunction(xml) {
     var table = "<tr><th>Artist</th><th>Title</th></tr>";
     var x = xmlDoc.getElementsByTagName("CD");
 
-    for (i = 0; i < x.length; i++){
+    for (i = 0; i < x.length; i++) {
         table += "<tr><td>" +
             x[i].getElementsByTagName("ARTIST"[0]).childNodes[0].nodeValue +
             x[i].getElementsByTagName("TITLE"[0]).childNodes[0].nodeValue +
             "</td></tr>";
     }
-        document.getElementById("demo").innerHTML = table;
+    document.getElementById("demo").innerHTML = table;
 
 }
+
+    app.listen(3000, "127.0.0.1");
+
+    console.log("Node server running on port 3000");
+
