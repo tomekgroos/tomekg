e17997cb9aa52b58b772884f3a2f865681170ec3
TomekG <tomek.groos@gmail.com>
2019-12-27 19:16:20 +0100

    dolaczenie pd1

--- Praca domowa/PD1/pd1.js
+++ Praca domowa/PD1/pd1.js
@@ -1,13 +1,25 @@
 
-function testJs() {
+    var textArray = ["Audi", "BMW", "Mercedes", "Ford", "Chevrolet"];
 
-    var textArray = ["pierwsza", "wartosc", "tekstowa", "druga", "wartosc"];
+    document.write("Tablica przed zmiana:</br>");
 
     for (var i = 0; i < textArray.length; i++){
 
-        console.log(textArray[i]);
+        document.write(textArray[i] + " ");
+    }
+
+    function switchElement() {
+        textArray[0] = prompt("Podaj pierwszy element tablicy do zmiany: ");
+
+        document.write("Tablica po zmianie:</br>");
+
+        for (var i = 0; i < textArray.length; i++){
+
+            document.write(textArray[i] + " ");
+        }
     }
 
 
 
-}
+
+
