b6823ac38c4aa65cfa27a3b40be0ef48df094136
TomekG <tomek.groos@gmail.com>
2019-12-27 18:35:10 +0100

    dolaczenie pd1

---
+++ Praca domowa/PD1/pd1.js
@@ -0,0 +1,13 @@
+
+function testJs() {
+
+    var textArray = ["pierwsza", "wartosc", "tekstowa", "druga", "wartosc"];
+
+    for (var i = 0; i < textArray.length; i++){
+
+        console.log(textArray[i]);
+    }
+
+
+
+}
