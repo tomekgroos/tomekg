3732ff1e2d5ae4451e7d63cb146d9bc71e5dfa93
Your Name <you@example.com>
2020-01-14 12:45:54 +0100

    gotowy kalkulator

--- calculator/calc.js
+++ calculator/calc.js
@@ -58,6 +58,8 @@ const performCalculation = {
 
     '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
 
+    '%': (firstOperand, secondOperand) => firstOperand % secondOperand,
+
     '=': (firstOperand, secondOperand) => secondOperand
 };
 
@@ -103,3 +105,114 @@ keys.addEventListener('click', (event) => {
     inputDigit(target.value);
     updateDisplay();
 });
+
+// wprowadzanie wartości z klawiatury
+
+window.addEventListener("keydown", checkPressedKey);
+
+function checkPressedKey(key){
+
+// wprowadzanie cyfr
+
+    if (key.keyCode == "48" || key.keyCode == "96"){
+
+        inputDigit(key.value = "0");
+    }
+
+    if (key.keyCode == "49" || key.keyCode == "97"){
+
+        inputDigit(key.value = "1");
+    }
+
+    if (key.keyCode == "50" || key.keyCode == "98"){
+
+        inputDigit(key.value = "2");
+    }
+
+    if (key.keyCode == "51" || key.keyCode == "99"){
+
+        inputDigit(key.value = "3");
+    }
+
+    if (key.keyCode == "52" || key.keyCode == "100"){
+
+        inputDigit(key.value = "4");
+    }
+
+    if (key.keyCode == "53" || key.keyCode == "101"){
+
+        inputDigit(key.value = "5");
+    }
+
+    if (key.keyCode == "54" || key.keyCode == "102"){
+
+        inputDigit(key.value = "6");
+    }
+
+    if (key.keyCode == "55" || key.keyCode == "103"){
+
+        inputDigit(key.value = "7");
+    }
+
+    if (key.keyCode == "56" || key.keyCode == "104"){
+
+        inputDigit(key.value = "8");
+    }
+
+    if (key.keyCode == "57" || key.keyCode == "105"){
+
+        inputDigit(key.value = "9");
+    }
+
+// wprowadzanie operatorów
+
+if (key.keyCode == "187" || key.keyCode == "107"){
+
+    handleOperator(key.value = "+");
+}
+
+if (key.keyCode == "189" || key.keyCode == "109"){
+
+    handleOperator(key.value = "-");
+}
+
+if (key.keyCode == "106"){
+
+    handleOperator(key.value = "*");
+}
+
+if (key.keyCode == "191" || key.keyCode == "111"){
+
+    handleOperator(key.value = "/");
+}
+
+// znak modulo pod przyciskiem 'm'
+
+if (key.keyCode == "77"){
+
+    handleOperator(key.value = "%")
+}
+
+// znak rownosci po enter
+
+if (key.keyCode == "13"){
+
+    handleOperator(key.value = "=");
+}
+
+// wprowadzanie przecinka
+
+if (key.keyCode == "190" || key.keyCode == "110" ){
+
+    inputDecimal(key.value = ".");
+}
+
+// czyszczenie ekranu pod delete
+
+if (key.keyCode == "46"){
+
+    resetCalculator();
+}
+
+    updateDisplay();
+}
